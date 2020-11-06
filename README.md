f7a9acb2d300d64155ef22e5d8ef153485460399
parkerbenoit <72704200+parkerbenoit@users.noreply.github.com>
2020-11-06 14:34:59 -0800

    readme mostly filled out

--- README.md
+++ README.md
@@ -8,10 +8,9 @@
 
 ## Description
 
-
+This project is a webpage for Parker's Pizza Parlor, where you can order a pizza! Options inclue pizza size and several topping choices. Upon entry of pizza specifications, a grand total price will be presented.
 
 ## Setup/Installation Requirements
-
 
 
 ## Specs / Tests
@@ -22,17 +21,17 @@
 | Name and Phone inputs should log| Parker Benoit, 206 123 4567 | Parker Benoit 206 123 4567 |
 | Select pizza size | pizza size | returns size user chose from checkbox|
 | Pick pizza toppings | toppings | returns toppings user chose from checkboxes |
-
+| Return overall price of order | complete results from our html form with inputs from the checklist | a total price added together in regards to size and toppings chosen by user |
 
 
 
 ## Known Bugs
 
-
+Currently don't have user input displayed on page after clicking submit.
 
 ## Contact Details
 
-
+email: parker.benoit44@gmail.com
 
 ## Technologies Used
 
--- js/scripts.js
+++ js/scripts.js
@@ -18,7 +18,7 @@ TotalOrder.prototype.assignId = function() {
 
 function Pizza(size) {
   this.size = size;
-  this.toppings = []
+  this.toppings = [];
 }
 
 Pizza.prototype.addToppings = function(topping) {
@@ -27,7 +27,6 @@ Pizza.prototype.addToppings = function(topping) {
 
 Pizza.prototype.getPrice = function() {
   let price = 0;
-}
 
   if(this.size === "small") {
     price = 10;
@@ -41,6 +40,25 @@ Pizza.prototype.getPrice = function() {
     price = 26;
   }
 
+  this.toppings.forEach(function() {
+    price +=1;
+  });
+
+  console.log("price: ", price);
+    return price;
+}
+
+TotalOrder.prototype.TotalPrice = function() {
+  let totalPrice = 0;
+  this.pizzas.forEach(function(pizza) {
+    totalPrice += pizza.getPrice();
+    this.totalPrice = totalPrice ;
+  });
+  return totalPrice;
+}
+
+
+
 
 
 $(document).ready(function() {
@@ -49,7 +67,7 @@ $(document).ready(function() {
     const size;
     //checkbox info
     $("input:checkbox[name=size]:checked").each(function() {
-      const inputtedSize = $(this).val().[0];
+      const inputtedSize = $(this).val().split("-")[0];
       size = inputtedSize;
     });
   });
