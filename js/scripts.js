a6ec6ad8b5983462999cd2c141ba7df874fa7deb
parkerbenoit <72704200+parkerbenoit@users.noreply.github.com>
2020-11-06 10:13:40 -0800

    previous commits were in wrong branch

--- js/scripts.js
+++ js/scripts.js
@@ -11,16 +11,25 @@ function Pizza(size) {
   this.size = size;
 }
 
+Pizza.prototype.getPrice = function() {
+  let price = 0;
+}
 
-if(this.size === "small") {
-  price = 10;
-} else if (this.size === "medium") {
-  price = 14
-} else if (this.size === "large") {
-  price = 18
-} else if (this.size === "XL") {
-  price = 22
-} 
+  if(this.size === "small") {
+    price = 10;
+  } else if (this.size === "medium") {
+    price = 14;
+  } else if (this.size === "large") {
+    price = 18;
+  } else if (this.size === "XL") {
+    price = 22;
+  } else if (this.size === "XXL") {
+    price = 26;
+  }
+
+  console.log("price: " price);
+    return price;
+}
 
 
 // User Interface Logic
